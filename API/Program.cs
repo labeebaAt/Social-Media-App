f578aef65dc2fdfe5c1ad34be2310a2b779a088c
Labeeba <[labeelola@gmail.com]0>
2022-03-30 09:35:36 +0600

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors();
 //Specify DataContext name of the class that derives from DbContext
 builder.Services.AddDbContext<DataContext>(options =>
 {
@@ -27,6 +27,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthorization();
 
